52ecf6a0ca4c74f4c6422d62ed9f63f415d0283b
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-08-16 14:48:53 +0500

    complete assignment_04

---
+++ assignment_04/main.py
@@ -0,0 +1,39 @@
+# function to check whether a given number is even or odd
+def is_even_or_odd(num: int) -> str:
+    if num % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+
+
+# declare variables
+user_name: str
+favorite_numbers: list[int] = []
+
+# initialize variables after user input
+user_name = input("Enter your name: ")
+first_favorite_number: int = int(input("Enter your first favorite number: "))
+second_favorite_number: int = int(input("Enter your second favorite number: "))
+third_favorite_number: int = int(input("Enter your third favorite number: "))
+
+# appending the user's favorite numbers to the "favorite_numbers" list
+favorite_numbers.append(first_favorite_number)
+favorite_numbers.append(second_favorite_number)
+favorite_numbers.append(third_favorite_number)
+
+# ___________________________________________________________________
+
+print(f"\nHello, {user_name}! Let's explore your favorite numbers:\n")
+
+# ___________________________________________________________________
+
+for number in favorite_numbers:
+    print(f"The number {number} is {is_even_or_odd(number)}.")
+
+print("\n")  # line break
+
+for number in favorite_numbers:
+    print(f"The number {number} and it's square: {number, number ** 2}")
+# ___________________________________________________________________
+
+print(f"\nAmazing! The sum of your favorite numbers is: {sum(favorite_numbers)}")
