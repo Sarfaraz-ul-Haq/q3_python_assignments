c52c3ac14a0504b1415b315e938b9371b30583b9
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-08-24 15:24:51 +0500

    complete assignment_06

--- assignment_06/main.py
+++ assignment_06/main.py
@@ -4,7 +4,6 @@ from typing import Tuple
 
 def manage_student_database():
     student_list: List[str] = []
-    # id: list[int] = list(range(1, len(student_list) + 1))
 
     while True:
         student_name: str = input(
@@ -23,6 +22,10 @@ def manage_student_database():
         else:
             student_list.append(student_name)
 
+    student_list_without_spaces: List[str] = list(
+        map(lambda student: student.replace(" ", ""), student_list)
+    )
+
     student_names_with_ids: Tuple[int, str] = list(enumerate(student_list, start=1))
     print(f"\nComplete list of students:\n{student_names_with_ids}\n")
 
@@ -33,18 +36,14 @@ def manage_student_database():
     total_students: int = len(student_list)
     print(f"\nTotal number of students: {total_students}")
 
-    student_names_length: int = len("".join(student_list))
+    student_names_length: int = len("".join(student_list_without_spaces))
     print(f"Total length of all student names combined: {student_names_length}")
 
-    longest_name: str = max(student_list, key=len)
+    longest_name: str = max(student_list_without_spaces, key=len)
     print(f"The student with the longest name is: {longest_name}")
 
-    shortest_name: str = min(student_list, key=len)
+    shortest_name: str = min(student_list_without_spaces, key=len)
     print(f"The student with the shortest name is: {shortest_name}")
 
 
 manage_student_database()
-
-# Todo
-# Dont' append if the user enters without typing anything
-# It should exclude spaces between names also
